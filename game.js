74bcd3817550ca744705541755b4d40e90f69c4e
Tatiana Penkova <tatianapenkova@gmail.com>
2020-04-21 22:07:34 +0300

    third task

---
+++ game.js
@@ -0,0 +1,18 @@
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
+
+  import Character from './domain.js';
+  
+  export { Game, GameSavingData, readGameSaving, writeGameSaving }
